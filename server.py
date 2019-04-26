2e5204c697be5011e64180ce39606731ce53b4e8
Kishor Subedi <kishbuy238@gmail.com>
2019-04-25 21:05:25 -0700

    Update server.py

--- server.py
+++ server.py
@@ -2,28 +2,45 @@
 import socket
 import sys
 
-HOST = None          # Symbolic name meaning all available interfaces
+HOST = 'localhost'         # Symbolic name meaning all available interfaces
 PORT = 8888             # Arbitrary non-privileged port
-s = None
-for res in socket.getaddrinfo(HOST, PORT, socket.AF_UNSPEC,
-                              socket.SOCK_STREAM, 0, socket.AI_PASSIVE):
-    af, socktype, proto, canonname, sa = res
-    try:
-        s = socket.socket(af, socktype, proto)
-    except OSError as msg:
-        s = None
-        continue
-    try:
-        s.bind(sa)
-        s.listen(1)
-    except OSError as msg:
-        s.close()
-        s = None
-        continue
-    break
-if s is None:
-    print('could not open socket')
-    sys.exit(1)
+
+COMMAND_BUFFER_SIZE = 256
+
+def GetIPv6Addr(addr, port):
+    # Try to detect whether IPv6 is supported at the present system and
+    # fetch the IPv6 address.
+    if not socket.has_ipv6:
+        raise Exception("the local machine has no IPv6 support enabled")
+
+    addrs = socket.getaddrinfo(addr, port, socket.AF_INET6, 0, socket.SOL_TCP)
+
+    if len(addrs) == 0:
+        raise Exception("there is no IPv6 address configured for the address")
+
+    return addrs[0][-1]
+
+
+def CreateServerSocket(addr, port):
+    """Creates a socket that listens on a specified port.
+
+    Args:
+        addr: ip address or "localhost" which will be used as a listening
+                socket for the server.
+        port: int from 0 to 2^16. Low numbered ports have defined purposes. Almost
+                all predefined ports represent insecure protocols that have died out.
+    Returns:
+        An socket that implements TCP/IP.
+    """
+    sockaddr = GetIPv6Addr(addr, port)
+
+    server = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
+    server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    server.bind(sockaddr)
+    server.listen()
+    return server
+
+s = CreateServerSocket(HOST, PORT)
 conn, addr = s.accept()
 with conn:
     print('Connected by', addr)
