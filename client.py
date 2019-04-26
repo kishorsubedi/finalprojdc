b5e6cc719196535a3c125d05b5dbded330da4853
Kishor Subedi <kishbuy238@gmail.com>
2019-04-25 20:48:22 -0700

    Update client.py

--- client.py
+++ client.py
@@ -1,13 +1,28 @@
-#!/usr/bin/env python3
-
+# Echo client program
 import socket
+import sys
 
-HOST = 'fd8c:624e:ee6f:0:2017:6e93:f788:fc40'  # The server's hostname or IP address
-PORT = 8888     # The port used by the server
-
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
-    s.sendall("hello".encode())
+HOST = 'fd8c:624e:ee6f:0:2017:6e93:f788:fc40'    # The remote host
+PORT = 8888              # The same port as used by the server
+s = None
+for res in socket.getaddrinfo(HOST, PORT, socket.AF_UNSPEC, socket.SOCK_STREAM):
+    af, socktype, proto, canonname, sa = res
+    try:
+        s = socket.socket(af, socktype, proto)
+    except OSError as msg:
+        s = None
+        continue
+    try:
+        s.connect(sa)
+    except OSError as msg:
+        s.close()
+        s = None
+        continue
+    break
+if s is None:
+    print('could not open socket')
+    sys.exit(1)
+with s:
+    s.sendall(b'Hello, world')
     data = s.recv(1024)
-
 print('Received', repr(data))
